443aa67fe19a0c07d32b8774b322740ea994f7b8
David Fuller <david@muvi2.co.uk>
2023-05-22 11:03:17 +0100

    More globals

--- globals.jsx
+++ globals.jsx
@@ -3,14 +3,53 @@ var settingsFilenameJson = "settings.json"
 var settingsFolder = File($.fileName).parent.fsName + "/settings/";
 var macXMLFolder = "/Users/David/Dropbox/Development/Adobe/Excel/";
 var pcXMLFolder = "/C/Users/David/Documents/vscode/Adobe_Versioning/xml/";
+var macOutputFolder = "/Users/David/Dropbox/Development/Adobe/Movies/";
+var pcOutputFolder = "/C/Users/David/Documents/vscode/Adobe_Versioning/movies/";
+var logFolder = File($.fileName).parent.fsName + "/logs/";
+var logFilename = "log-YYYY-MM-DD.json"
 
 var compProfileFileName = "comp_profile.json"
-var textLayerNames = ["Title", "Message", "Navigation"]
+//var textLayerNames = ["1L Title", "1L Message", "1L Navigation", "2L Title", "2L Message", "2L Navigation"]
 var renderNames = ["Still", "Clip"]
+
+var textMapping = [
+  {
+    name: "1L Title",
+    profileField: "1L Title",
+    dataField: "oneLineTitle"
+  },
+  {
+    name: "1L Message",
+    profileField: "1L Message",
+    dataField: "oneLineMessage"
+  },
+  {
+    name: "1L Navigation",
+    profileField: "1L Navigation",
+    dataField: "oneLineNavigation"
+  },
+  {
+    name: "2L Title",
+    profileField: "2L Title",
+    dataField: "twoLineTitle"
+  },
+  {
+    name: "2L Message",
+    profileField: "2L Message",
+    dataField: "twoLineMessage"
+  },
+  {
+    name: "2L Navigation",
+    profileField: "2L Navigation",
+    dataField: "twoLineNavigation"
+  }
+]
 
 var defaultSettings = {
   macXMLPath: macXMLFolder,
-  pcXMLPath: pcXMLFolder
+  pcXMLPath: pcXMLFolder,
+  macOutputPath: macOutputFolder,
+  pcOutputPath: pcOutputFolder
 }
 
 function getXMLFolder(){
@@ -20,3 +59,12 @@ function getXMLFolder(){
     return new Folder(theSettings.macXMLPath);
   }
 }
+function getOutputFolderName(){
+  if (Folder.fs == "Windows"){
+    return theSettings.pcOutputPath;
+  } else {
+    return theSettings.macOutputPath;
+  }
+}
+
+var archiveSubFolder = "archive/"
